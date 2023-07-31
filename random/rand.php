3b2cc728feed74ce5913b9d7e5f8e5c237b78836
y.ivanova@automacon.ru <ivanova.julia.1997@mail.ru>
2023-07-31 14:33:32 +0300

    добавлена форма генерации слов

---
+++ random/rand.php
@@ -0,0 +1,5 @@
+<?php
+$anyWordsArray = array('Hello', 'July', 'Test', 'World', 'LOL');
+$rand_key=array_rand($anyWordsArray,1);
+echo $anyWordsArray[$rand_key];
+?>
--- random/random.php
+++
@@ -1,6 +0,0 @@
-<?php
-$min=1;
-$max=100;
-$n=rand($min,$max);
-echo $n;
-?>
